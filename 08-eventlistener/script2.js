47d036fbf0a021fcedd277c4f6b99fbf619e7e71
ganglinwu <ganglin.wu@gmail.com>
2023-05-19 10:38:15 +0800

    Added more buttons and console logging

--- 08-eventlistener/script2.js
+++ 08-eventlistener/script2.js
@@ -8,4 +8,21 @@ console.log(e) --> This prints PointerEvent into console
 console.log(e.target) --> This prints the html tags of the button being clicked
 */
 
-document.createElement
+var btn2 = document.createElement('button');
+btn2.setAttribute('id', 'btn2');
+var btn3 = document.createElement('button');
+btn3.setAttribute('id', 'btn3');
+
+btn2.textContent = 'Click me! (2)';
+btn3.textContent = 'Click me! (3)';
+
+wrapper1.appendChild(btn2);
+wrapper1.appendChild(btn3);
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    });
+});
