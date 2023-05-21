459f36e1b5d9ca619a935b575403df1b86eed05b
ganglinwu <ganglin.wu@gmail.com>
2023-05-22 00:48:52 +0800

    Implemented first to 5 wins feature

--- 06-rock-paper-scissors/rps.js
+++ 06-rock-paper-scissors/rps.js
@@ -1,15 +1,39 @@
 let rpsArray = ['rock', 'paper', 'scissors'];
 
 // let's initialize some counters for stats
-var gamesPlayed = playerWins = computerWins = winPercentage = 0;
+var gamesPlayed = 0;
+var playerWins = 0;
+var computerWins = 0;
+var winPercentage = 0;
+
 
 function computerChoice() {
     let comChoice = rpsArray.at(Math.floor(Math.random()*10)%3); //generates random number 0, 1 or 2
     return comChoice;
-}    
+}
+
+// event listener for buttons
+var rockbtn = document.querySelector('#rockbtn');
+var paperbtn = document.querySelector('#paperbtn');
+var scissorsbtn = document.querySelector('#scissorsbtn');
+
+
+rockbtn.addEventListener('click', () => {
+    RPS('rock');
+});
+
+paperbtn.addEventListener('click', () => {
+    RPS('paper');
+});
+
+scissorsbtn.addEventListener('click', () => {
+    RPS('scissors');
+});
+
+
 
 function RPS(playerChoice) {
-
+    
     // RPS win 3 different values
     // -1 means computer win
     // 0 means draw or tie
@@ -22,12 +46,12 @@ function RPS(playerChoice) {
     // playerChoice will be user input from html page
     // computerChoice will be from function above
     let compChoice = computerChoice(); 
-
-
+    
+    
     // to compare rock, paper, scissors let's implement values
     // by using values we can shorten the number of cases to consider
     let playerValue, computerValue = 0;
-
+    
     // assign rock = 0 
     // paper = 1
     // scissors = 2 
@@ -39,14 +63,14 @@ function RPS(playerChoice) {
             computerValue = i;
         }    
     }        
-
+    
     // below we determine winner using value system
     let difference = playerValue - computerValue;
-
+    
     if (playerValue === computerValue) {
         score = 0;
     }
-
+    
     else if (Math.abs(difference) === 2) {
         if (difference < 0) {
             score = 1;
@@ -67,26 +91,51 @@ function RPS(playerChoice) {
     }
     
     // let's update html page with computer choice
-
-    document.getElementById('comChoice').innerHTML = `Computer chose: ${compChoice}`;
-
+    
+    document.getElementById('comChoice').textContent = `Computer chose: ${compChoice}`;
+    
     // let's update stats and counters
     gamesPlayed += 1;
     if (score == 1) {
         playerWins += 1;
-        document.getElementById('winner').innerHTML = `You chose: ${playerChoice} <br> You WIN!`;
+        document.getElementById('winner').textContent = `You chose: ${playerChoice}.. You WIN!`;
     }
     else if (score == -1) {
         computerWins += 1;
-        document.getElementById('winner').innerHTML = `You chose: ${playerChoice} <br> You lost..`;
+        document.getElementById('winner').textContent = `You chose: ${playerChoice}.. You lost..`;
     }
     else {
-        document.getElementById('winner').innerHTML = `You chose: ${playerChoice} <br> It's a tie`;
+        document.getElementById('winner').textContent = `You chose: ${playerChoice}.. It's a tie`;
     }
     winPercentage = (playerWins/gamesPlayed*100).toFixed(2);
     
-    document.getElementById('gamesPlayed').innerHTML = `${gamesPlayed}`;
-    document.getElementById('playerScore').innerHTML = `${playerWins}`;
-    document.getElementById('comScore').innerHTML = `${computerWins}`;
-    document.getElementById('winPercentage').innerHTML = `${winPercentage}%`;
+    document.getElementById('gamesPlayed').textContent = `${gamesPlayed}`;
+    document.getElementById('playerScore').textContent = `${playerWins}`;
+    document.getElementById('comScore').textContent = `${computerWins}`;
+    document.getElementById('winPercentage').textContent = `${winPercentage}%`;
+
+    if (playerWins == 5) {
+        winnerIsPlayer();
+    } else if (computerWins == 5) {
+        winnerIsComputer();
+    }
+}
+
+
+function winnerIsPlayer() {
+    let buttons = document.querySelectorAll('button')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+    document.body.style.cssText = 'background-color:green;'
+    document.querySelector('h1').textContent = 'PLAYER WINS!!!'
+}
+
+function winnerIsComputer() {
+    let buttons = document.querySelectorAll('button')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+    document.body.style.cssText = 'background-color:red;'
+    document.querySelector('h1').textContent = 'Computer wins!!!'
 }
