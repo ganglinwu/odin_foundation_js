ab28815c91d4905282ef5effced7085e4708a7ad
ganglinwu <ganglin.wu@gmail.com>
2023-06-18 17:23:02 +0800

    Implemented Erase Mode feature

--- 10-etch-a-sketch/script.js
+++ 10-etch-a-sketch/script.js
@@ -1,11 +1,36 @@
 "use strict";
 
+let currentHoverColor='#7fffda'
+let currentDrawnColor='#66cdaa'
+
+
 let gridsize = 16;
 draw(gridsize);
+
+const boxes = document.getElementsByClassName('box-grid');
 
 /* ---------------------------------------------------*/
 /* ---------------- Draw Grid Feature ----------------*/
 /* ---------------------------------------------------*/
+
+/* helper function to darken color by 20% */
+function darkenColor(hexString) {
+    // split hex string into array with length 7
+    let hexArr = hexString.split('');
+
+    // splice from index 1(so that we skip # symbol) and take 2 elements
+    // note that splicing modifies hexArr, so we splice same section of arr everytime
+    // join them together, parse to integer from hexadecimal(base-16)
+    // multiply by 0.8 to darken, and round to whole number
+    let r = Math.floor(parseInt(hexArr.splice(1,2).join(''), 16)*0.8);
+    let g = Math.floor(parseInt(hexArr.splice(1,2).join(''), 16)*0.8);
+    let b = Math.floor(parseInt(hexArr.splice(1,2).join(''), 16)*0.8);
+
+    // convert back to hexadecimal, concatenate together with # symbol
+    return(`#${r.toString(16)}${g.toString(16)}${b.toString(16)}`)
+}
+
+
 
 /* function to draw grid */
 function draw(gridsize) {
@@ -19,13 +44,13 @@ function draw(gridsize) {
         
         for (let k = 0; k < gridsize; k++) {
             let box = document.createElement('div');
+            box.classList.add('box-grid');
             box.classList.add(`box${k+1},${16-i}`);
             box.style.cssText= 'border: 1px solid #f9f9f9; flex: 1; transition: 0.2s;'
             box.addEventListener('mouseover', ()=> {
-                box.classList.add('hover');});
+                box.style.background = `${currentHoverColor}`});
             box.addEventListener('mouseout', ()=> {
-                box.classList.remove('hover');
-                box.classList.add('drawn')});
+                box.style.background = `${currentDrawnColor}`});
             rowDiv.appendChild(box);
         }
         container.appendChild(rowDiv);
@@ -82,7 +107,7 @@ function confirmEraseALL() {
 
 /* event listener for click*/
 const eraseAllbtn = document.getElementsByClassName('erase-all')[0]; //getElementsbyClass returns HTMLCollection(Array like object) even when there's only one element!
-eraseAllbtn.addEventListener('click', confirmEraseALL)
+eraseAllbtn.addEventListener('click', confirmEraseALL);
 
 /* event listener for keypress*/
 window.addEventListener('keypress', (e)=> {
@@ -99,3 +124,57 @@ window.addEventListener('keypress', (e)=> {
 /* ---------------- End of Erase All Feature ----------------*/
 /* ----------------------------------------------------------*/
 
+
+/* ----------------------------------------------------*/
+/* ---------------- Erase Mode Feature ----------------*/
+/* ----------------------------------------------------*/
+const eraseModebtn = document.getElementsByClassName('erase-mode')[0];
+
+/* erase mode function*/
+function eraseMode() {
+    if (eraseModebtn.classList.contains('selected')) {
+        currentDrawnColor = darkenColor(currentHoverColor);
+        eraseModebtn.classList.remove('selected');
+        Array.from(boxes).forEach((box)=>
+            box.addEventListener('mouseover', ()=> {
+                box.style.background = `${currentHoverColor}`;
+            })
+        );
+        Array.from(boxes).forEach((box)=>
+            box.addEventListener('mouseout', ()=> {
+                box.style.background = `${currentDrawnColor}`;
+            })
+        );
+    }
+    else {
+        eraseModebtn.classList.add('selected')
+        Array.from(boxes).forEach((box)=>
+            box.addEventListener('mouseover', ()=> {
+                box.style.background = '#a7a7a7';
+            })
+        );
+        Array.from(boxes).forEach((box)=>
+            box.addEventListener('mouseout', ()=> {
+                box.style.background = '#ffffff';
+            })
+        );
+    }
+}
+
+/* event listener for click */
+eraseModebtn.addEventListener('click', eraseMode);
+
+/* event listener for keypress */
+window.addEventListener('keypress', (e)=> {
+    if (e.key.toLowerCase()=='q') {
+        eraseMode();
+    }
+    else {
+        return;
+    }
+})
+
+
+/* -----------------------------------------------------------*/
+/* ---------------- End of Erase Mode Feature ----------------*/
+/* -----------------------------------------------------------*/
